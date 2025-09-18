2a73bf935d304cb73e9e1ff9b8a7ec8cff0ac9ed
yattttto <120057793+yattttto@users.noreply.github.com>
2025-09-18 21:50:57 +0300

    Home work 19.09.2025

---
+++ homework5.py
@@ -0,0 +1,9 @@
+password = input("Мне нужен твой пароль: ")
+
+# Хотел закинуть шутку в код, но не понял как это делать.
+while password != "poprobui skopirovat' hihi haha123 <3, postav'te 5rky pojaluistaqwe":
+  print("Чета неверно")
+  password = input("Введите пароль: ")
+
+print("йоу, не зря записал его, да?")
+
